045538d6ce62d7a161368099441bcc78aae4a4f0
Felix <jonfelixrico@gmail.com>
2023-07-19 01:11:59 +0800

    Create page container

---
+++ src/app/reader/PageContainer.tsx
@@ -0,0 +1,20 @@
+import { Dimensions } from '../types/dimensions.interface'
+
+export default function PageContainer({
+  dimensions: { height, width },
+  src,
+}: {
+  dimensions: Dimensions
+  src: string
+}) {
+  return (
+    <div
+      style={{
+        height,
+        width,
+      }}
+    >
+      <img src={src} />
+    </div>
+  )
+}
