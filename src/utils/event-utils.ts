346b5e8dc1b5d59a1348cc39f9c0324f323808d4
Felix <jonfelixrico@gmail.com>
2023-07-25 22:07:43 +0800

    Remove unused

--- src/utils/event-utils.ts
+++
@@ -1,9 +0,0 @@
-export function isEventTargetWithinElement(
-  containingElement: Element,
-  eventTarget: EventTarget | null
-) {
-  return (
-    containingElement === eventTarget ||
-    (eventTarget instanceof Element && containingElement.contains(eventTarget))
-  )
-}
