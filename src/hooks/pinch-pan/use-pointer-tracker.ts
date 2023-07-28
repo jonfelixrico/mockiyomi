9d8c7c651667e4a9bc4895eb1fe27ee009e42fd7
Felix <jonfelixrico@gmail.com>
2023-07-29 02:08:40 +0800

    Use array instead of map

--- src/hooks/pinch-pan/use-pointer-tracker.ts
+++ src/hooks/pinch-pan/use-pointer-tracker.ts
@@ -1,29 +1,38 @@
 import { useState } from 'react'
 
 export function usePointerTracker() {
-  const [pointerMap, setPointerMap] = useState<Record<string, PointerEvent>>({})
+  const [pointerCache, setPointerCache] = useState<PointerEvent[]>([])
 
   function setPointer(e: PointerEvent) {
-    setPointerMap((map) => {
-      return {
-        ...map,
-        [e.pointerId]: e,
+    setPointerCache((cache) => {
+      const index = cache.findIndex((p) => p.pointerId === e.pointerId)
+      if (index === -1) {
+        return [...cache, e]
       }
+
+      // replace the old record of the pointer with the new one
+      return cache.slice().splice(index, 1, e)
     })
   }
 
   function removePointer(e: PointerEvent) {
-    setPointerMap((map) => {
-      const clone = { ...map }
-      delete clone[e.pointerId]
+    setPointerCache((cache) => {
+      const index = cache.findIndex((p) => p.pointerId === e.pointerId)
 
-      return clone
+      if (index === -1) {
+        // pointer not found so do nothing
+        return cache
+      }
+
+      // remove pointer from the cache
+      return cache.slice().splice(index, 1)
     })
   }
 
   return {
-    pointerCount: Object.keys(pointerMap).length,
+    pointerCount: pointerCache.length,
     setPointer,
     removePointer,
+    originPointer: pointerCache[0]?.pointerId ?? null,
   }
 }
