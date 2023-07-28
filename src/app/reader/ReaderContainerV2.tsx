f74d12339b9a5d2715272e990c00f076653d5ed2
Felix <jonfelixrico@gmail.com>
2023-07-29 01:54:50 +0800

    Re-integrate

--- src/app/reader/ReaderContainerV2.tsx
+++ src/app/reader/ReaderContainerV2.tsx
@@ -21,16 +21,16 @@ export default function ReaderContainerV2(props: {
 
   const ref = useRef<null | HTMLDivElement>(null)
 
-  // usePinchPan(ref, ({ isFinal, panDelta }) => {
-  //   if (isFinal) {
-  //     setTranslateX(0)
-  //   } else {
-  //     setTranslateX((val) => {
-  //       const newGross = val + panDelta.x
-  //       return Math.min(Math.max(-props.dims.width, newGross), props.dims.width)
-  //     })
-  //   }
-  // })
+  usePinchPan(ref, ({ isFinal, panDelta }) => {
+    if (isFinal) {
+      setTranslateX(0)
+    } else {
+      setTranslateX((val) => {
+        const newGross = val + panDelta.x
+        return Math.min(Math.max(-props.dims.width, newGross), props.dims.width)
+      })
+    }
+  })
 
   return (
     <div
