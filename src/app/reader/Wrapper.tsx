71a6186e9a1d5b5ce8adc5f59ab7834273903488
Felix <jonfelixrico@gmail.com>
2023-07-30 01:40:03 +0800

    Integrate into Wrapper temporarily

--- src/app/reader/Wrapper.tsx
+++ src/app/reader/Wrapper.tsx
@@ -2,6 +2,7 @@
 
 import { useMeasure } from 'react-use'
 import ReaderContainerV2 from './ReaderContainerV2'
+import PageViewer from './PageViewer'
 
 const URLS = [
   '/placeholder/1.jpg',
@@ -14,12 +15,13 @@ export default function Wrapper() {
   const [ref, dims] = useMeasure<HTMLDivElement>()
   return (
     <div ref={ref} className="h-screen w-screen overflow-hidden">
-      <ReaderContainerV2
+      {/* <ReaderContainerV2
         dims={dims}
         current={URLS[1]}
         next={URLS[2]}
         prev={URLS[0]}
-      />
+      /> */}
+      <PageViewer dimensions={dims} src={URLS[0]} />
     </div>
   )
 }
