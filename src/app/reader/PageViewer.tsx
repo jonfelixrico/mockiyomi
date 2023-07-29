1d1c940765bd8064dc09ae6b388db9c8e6ea029b
Felix <jonfelixrico@gmail.com>
2023-07-30 01:36:16 +0800

    Integrate

--- src/app/reader/PageViewer.tsx
+++ src/app/reader/PageViewer.tsx
@@ -2,16 +2,65 @@
 
 import { Dimensions } from '@/types/dimensions.interface'
 import PageScroller, { ScrollPosition } from './PageScroller'
-import { RefObject, useRef, useState } from 'react'
+import { RefObject, useMemo, useRef, useState } from 'react'
 import ImgWrapper from './ImgWrapper'
+import { usePinchPan } from '@/hooks/pinch-pan/use-pinch-pan'
 
-function usePinchPanInterface(
-  ref: RefObject<HTMLDivElement>,
-  pageDims: Dimensions
-) {
+function clampValue(value: number, limits: { min: number; max: number }) {
+  const minBound = Math.max(limits.min, value) // prevent value from dipping below the min limit
+  return Math.min(minBound, limits.max) // prevent value from going past the max limit
+}
+
+function getScrollLimits(content: Dimensions, container: Dimensions) {
+  return {
+    top: {
+      min: 0,
+      max: content.height - container.height,
+    },
+
+    left: {
+      min: 0,
+      max: content.height - container.height,
+    },
+  }
+}
+
+function useScrolling(pageDims: Dimensions, containerDims: Dimensions) {
   const [scroll, setScroll] = useState<ScrollPosition>({
     left: 0,
     top: 0,
+  })
+
+  const scrollLimits = getScrollLimits(pageDims, containerDims)
+
+  return {
+    scroll,
+    setScroll(callback: (position: ScrollPosition) => ScrollPosition) {
+      setScroll((value) => {
+        const raw = callback(value)
+        return {
+          top: clampValue(raw.top, scrollLimits.top),
+          left: clampValue(raw.left, scrollLimits.left),
+        }
+      })
+    },
+  }
+}
+
+function usePinchPanInterface(
+  ref: RefObject<HTMLDivElement>,
+  pageDims: Dimensions,
+  containerDims: Dimensions
+) {
+  const { scroll, setScroll } = useScrolling(pageDims, containerDims)
+
+  usePinchPan(ref, ({ panDelta }) => {
+    setScroll((val) => {
+      return {
+        top: val.top + panDelta.y,
+        left: val.left + panDelta.x,
+      }
+    })
   })
 
   return {
@@ -31,7 +80,7 @@ export default function PageViewer({
   })
 
   const ref = useRef<HTMLDivElement>(null)
-  const { scroll } = usePinchPanInterface(ref, pageDims)
+  const { scroll } = usePinchPanInterface(ref, pageDims, props.dimensions)
 
   return (
     <div ref={ref}>
