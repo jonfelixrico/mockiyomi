fd44ebe4d381e26e47cbfa21545e3105121c2881
Felix <jonfelixrico@gmail.com>
2023-07-29 20:08:26 +0800

    Integrate into the reader for testing

--- src/app/reader/ReaderContainerV2.tsx
+++ src/app/reader/ReaderContainerV2.tsx
@@ -18,25 +18,25 @@ export default function ReaderContainerV2(props: {
 }) {
   const [translateX, setTranslateX] = useState(0)
   const [scale, setScale] = useState(1)
-  const [lastDelta, setLastDelta] = useState(1)
+  const [tempScale, setTempScale] = useState<number | null>(null)
 
   const ref = useRef<null | HTMLDivElement>(null)
 
-  usePinchPan(ref, ({ isFinal, panDelta, pinchDelta }) => {
-    if (isFinal) {
-      setTranslateX(0)
-      setScale(1)
-    } else {
-      setTranslateX((val) => {
-        const newGross = val + panDelta.x
-        return Math.min(Math.max(-props.dims.width, newGross), props.dims.width)
-      })
+  usePinchPan(ref, ({ isFinal, panDelta, pinch }) => {
+    // setTranslateX((val) => {
+    //   const newGross = val + panDelta.x
+    //   return Math.min(Math.max(-props.dims.width, newGross), props.dims.width)
+    // })
 
-      if (pinchDelta) {
-        setScale(pinchDelta)
+    if (pinch) {
+      if (pinch.isFinal) {
+        setTempScale(null)
+        setScale((scale) => {
+          return scale * (tempScale ?? 1)
+        })
+      } else {
+        setTempScale(pinch.delta)
       }
-
-      setLastDelta(pinchDelta)
     }
   })
 
@@ -51,31 +51,14 @@ export default function ReaderContainerV2(props: {
         transform: `translateX(${translateX}px)`,
       }}
     >
-      {props.prev ? (
-        <PageContainerV2
-          src={props.prev}
-          dimensions={props.dims}
-          className="absolute z-10 prev"
-        />
-      ) : null}
-
-      {props.next ? (
-        <PageContainerV2
-          src={props.next}
-          dimensions={props.dims}
-          className="absolute z-10 next"
-        />
-      ) : null}
-
       <div>
         <div className="absolute">
           <div>{scale}</div>
-          <div>{lastDelta}</div>
         </div>
         <PageContainerV2
           src={props.current}
           dimensions={props.dims}
-          scale={scale}
+          scale={tempScale ? scale * tempScale : scale}
         />
       </div>
     </div>
