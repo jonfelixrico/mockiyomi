739f514ed2c5973fb2f60742a1f1ddf699d3d99d
Felix <jonfelixrico@gmail.com>
2023-07-29 15:53:29 +0800

    Impl area detection

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,3 +3,10 @@
  * From https://stackoverflow.com/a/41946697
  */
 declare module '*.css'
+declare module 'convex-hull' {
+  function getConvexHull(
+    points: Array<[number, number]>
+  ): Array<[number, number]>
+
+  export default getConvexHull
+}
--- src/hooks/pinch-pan/point-utils.ts
+++ src/hooks/pinch-pan/point-utils.ts
@@ -1,4 +1,5 @@
 import { Point } from '@/types/point.interface'
+import convexHull from 'convex-hull'
 
 export function getCentroid(coords: Point[]): Point {
   let sumX = 0
@@ -20,4 +21,25 @@ export function getDistanceOfTwoPoints(a: Point, b: Point): number {
   const dy = Math.pow(b.y - a.y, 2)
 
   return Math.sqrt(dx + dy)
+}
+
+export function getAreaOfPoints(points: Point[]): number {
+  // get convex hull points
+  const chPoints = convexHull(points.map(({ x, y }) => [x, y])).map(
+    ([x, y]) => {
+      return { x, y }
+    }
+  )
+
+  /*
+   * Get area of points.
+   * Formula taken from https://www.mathopenref.com/coordpolygonarea.html
+   */
+  let area = 0
+  for (let i = 0; i < chPoints.length; i++) {
+    const a = chPoints[i]
+    const b = chPoints[(i + 1) % chPoints.length]
+    area += a.x * b.y - a.y * b.x
+  }
+  return Math.abs(area / 2)
 }
