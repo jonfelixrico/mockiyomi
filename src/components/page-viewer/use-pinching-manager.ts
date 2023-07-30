6523fe3084d42c4c33b932557d0ecafbbb284ebe
Felix <jonfelixrico@gmail.com>
2023-07-30 20:30:58 +0800

    Add comment

--- src/components/page-viewer/use-pinching-manager.ts
+++ src/components/page-viewer/use-pinching-manager.ts
@@ -37,30 +37,62 @@ export function usePinchingManager(
       return
     }
 
-    const refPoint = {
-      x: location.x + scroll.left,
-      y: location.y + scroll.top,
-    }
-
-    const refPointPercentage = {
-      x: refPoint.x / pageDims.width,
-      y: refPoint.y / pageDims.height,
+    /*
+     * This is just a representation of where the centerpoint of the pinch is.
+     * This will act as sort of an anchor.
+     */
+    const pinchAnchor = {
+      x: (location.x + scroll.left) / pageDims.width,
+      y: (location.x + scroll.left) / pageDims.height,
     }
 
     const tempScale = delta * persistedScale
 
+    /*
+     * This just computes the resulting dimensions of the page after the pinch
+     * instance.
+     *
+     * We're using bounded scale here because we're have min/max limits for the scale,
+     * and if the current scale just so surpasess those we will just bound them to the
+     * limits.
+     */
     const boundedScale = getBoundedScale(tempScale)
-    const afterResize = {
+    const dimsAfterResize = {
       width: (pageDims.width / scaleToDisplay) * boundedScale,
       height: (pageDims.height / scaleToDisplay) * boundedScale,
     }
 
-    const pointAfterResize = {
-      left: afterResize.width * refPointPercentage.x - location.x - panDelta.x,
-      top: afterResize.height * refPointPercentage.y - location.y - panDelta.y,
+    /*
+     * Consider this: you are in a photo viewing app for your phone. Say you have a photo of
+     * somebody and you are interesting in zooming in to see the details of their left eye.
+     *
+     * If you pinch in, the zooming appears to be "anchored" into the left eye -- whether you pinch
+     * to zoom in or out, the center point of your finger is still around the eye.
+     *
+     * That's the purpose of this scroll compensation -- to make it appear like the zoom is anchored
+     * to the point that the user is pinching at.
+     *
+     * The pan is just there to give panning capabilities to the user even though they're pinching.
+     */
+    const newScrollPositionForScalingCompensation = {
+      left: dimsAfterResize.width * pinchAnchor.x - location.x - panDelta.x,
+      top: dimsAfterResize.height * pinchAnchor.y - location.y - panDelta.y,
     }
+    setScroll(() => newScrollPositionForScalingCompensation)
 
-    setScroll(() => pointAfterResize)
+    /*
+     * For future reference, the key steps to the illusion above are:
+     *
+     * 1. Get the coordinates of where the pinch fingers of the user are. The coordinates are relative to the
+     *    subject being pinched-to-zoom before the actual scaling happens.
+     * 2. Determine top and left distances of the pinch fingers are from the scroll container. For image-viewing apps,
+     *    consider the top and left sides of the device screen as your scroll container.
+     * 3. Scale up the coordinates from step 1 accordingly to the new scale.
+     * 4. Set the scroll such that the top left corner of the scroll container is exactly on the point in step 3.
+     * 5. Add the top and left distance from step 2 to the scroll position in step 4.
+     *
+     * Viola, you will make it appear like the point didn't move in between scales.
+     */
 
     if (tempScale > MAX_SCALE) {
       setPinchSessionScale(MAX_SCALE / persistedScale)
