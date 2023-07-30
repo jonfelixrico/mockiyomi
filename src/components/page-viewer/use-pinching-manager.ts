82500c505b0763bc554550992f6a988fef7c90b9
Felix <jonfelixrico@gmail.com>
2023-07-30 17:34:23 +0800

    Externalize

---
+++ src/components/page-viewer/use-pinching-manager.ts
@@ -0,0 +1,77 @@
+import type { PinchEvent } from '@/hooks/pinch-pan/use-pinch-pan'
+import { Dimensions } from '@/types/dimensions.interface'
+import { ScrollPosition } from '@/types/scroll-location.interface'
+import { useMemo, useState } from 'react'
+
+const MAX_SCALE = 10
+const MIN_SCALE = 1
+function getBoundedScale(scale: number): number {
+  return Math.min(MAX_SCALE, Math.max(MIN_SCALE, scale))
+}
+
+export function usePinchingManager(
+  scroll: ScrollPosition,
+  setScroll: (callback: (position: ScrollPosition) => ScrollPosition) => void,
+  pageDims: Dimensions
+) {
+  const [persistedScale, setPersistedScale] = useState(1)
+  const [stagingScale, setStagingScale] = useState(1)
+  const actualScale = useMemo(
+    () => persistedScale * stagingScale,
+    [persistedScale, stagingScale]
+  )
+
+  function handlePinch({ delta, isFinal, isFirst, location }: PinchEvent) {
+    if (isFirst) {
+      /*
+       * Using the first is glitchy.
+       * TODO fix the functionality of the first.
+       */
+      return
+    }
+
+    if (isFinal) {
+      setPersistedScale(actualScale)
+      setStagingScale(1)
+      return
+    }
+
+    const refPoint = {
+      x: location.x + scroll.left,
+      y: location.y + scroll.top,
+    }
+
+    const refPointPercentage = {
+      x: refPoint.x / pageDims.width,
+      y: refPoint.y / pageDims.height,
+    }
+
+    const tempScale = delta * persistedScale
+
+    const boundedScale = getBoundedScale(tempScale)
+    const afterResize = {
+      width: (pageDims.width / actualScale) * boundedScale,
+      height: (pageDims.height / actualScale) * boundedScale,
+    }
+
+    const pointAfterResize = {
+      left: afterResize.width * refPointPercentage.x - location.x,
+      top: afterResize.height * refPointPercentage.y - location.y,
+    }
+
+    setScroll(() => pointAfterResize)
+
+    if (tempScale > MAX_SCALE) {
+      setStagingScale(MAX_SCALE / persistedScale)
+    } else if (tempScale < MIN_SCALE) {
+      setStagingScale(MIN_SCALE / persistedScale)
+    } else {
+      setStagingScale(delta)
+    }
+  }
+
+  return {
+    scale: actualScale,
+    handlePinch,
+  }
+}
