a626db9ffb78098e173547677ce82872e09e7b17
Felix <jonfelixrico@gmail.com>
2023-08-02 00:12:05 +0800

    Use math abs

--- src/components/page-viewer/use-scroll-limits.tsx
+++ src/components/page-viewer/use-scroll-limits.tsx
@@ -19,12 +19,18 @@ export function useScrollLimits(
     return {
       top: {
         min: 0,
-        max: pageDims.height - containerDims.height,
+        /*
+         * The computation inside abs can yield negative values. We need to properly detect
+         * which among the values are higher and lower to avoid negative values.
+         *
+         * Using Math.abs is a simpler way to prevent negative values.
+         */
+        max: Math.abs(pageDims.height - containerDims.height),
       },
 
       left: {
         min: 0,
-        max: pageDims.width - containerDims.width,
+        max: Math.abs(pageDims.width - containerDims.width),
       },
     }
   }, [pageDims, containerDims])
