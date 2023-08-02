392db4f2e3de2815b1caaed5b4caa64b71c5d152
Felix <jonfelixrico@gmail.com>
2023-08-03 00:00:07 +0800

    Create ImageWrapperV2

---
+++ src/components/page-viewer/ImgWrapperV2.tsx
@@ -0,0 +1,43 @@
+import {
+  CSSProperties,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+
+export default function ImgWrapperV2({
+  onRatioEmit,
+  ...props
+}: {
+  src: string
+  alt: string
+  style?: CSSProperties
+  className?: string
+  onRatioEmit: (ratio: number) => void
+}) {
+  const ref = useRef<HTMLImageElement>(null)
+
+  const getImageRatio = useCallback(() => {
+    const img = ref.current
+
+    if (img?.complete) {
+      onRatioEmit(img.naturalWidth / img.naturalHeight)
+    }
+  }, [ref, onRatioEmit])
+
+  useEffect(getImageRatio, [getImageRatio])
+
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={props.src}
+      alt={props.alt}
+      className={props.className}
+      ref={ref}
+      style={props.style}
+      onLoad={getImageRatio}
+    />
+  )
+}
