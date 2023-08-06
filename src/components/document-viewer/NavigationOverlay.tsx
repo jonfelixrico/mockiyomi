d68126763a580ef9c266c27afa2b53a563f5a21e
Felix <jonfelixrico@gmail.com>
2023-08-07 00:58:42 +0800

    Externalize the function

--- src/components/document-viewer/NavigationOverlay.tsx
+++ src/components/document-viewer/NavigationOverlay.tsx
@@ -4,6 +4,51 @@ import ConditionallyRender from '../common/ConditionallyRender'
 import { Button, Slider } from 'antd'
 import { VerticalRightOutlined, VerticalLeftOutlined } from '@ant-design/icons'
 
+function Controls({
+  setPageIndex,
+  pageCount,
+  pageIndex,
+}: {
+  pageCount: number
+  pageIndex: number
+  setPageIndex: Dispatch<number>
+}) {
+  const goNext = useCallback(() => {
+    setPageIndex(Math.min(pageCount - 1, pageIndex + 1))
+  }, [setPageIndex, pageIndex, pageCount])
+
+  const goPrev = useCallback(() => {
+    setPageIndex(Math.max(0, pageIndex - 1))
+  }, [setPageIndex, pageIndex])
+
+  return (
+    <div className="flex flex-row items-center">
+      <Button
+        type="primary"
+        shape="circle"
+        onClick={goPrev}
+        icon={<VerticalRightOutlined />}
+      />
+
+      <div className="grow">
+        <Slider
+          value={pageIndex + 1}
+          onChange={(value) => setPageIndex(value - 1)}
+          max={pageCount}
+          min={1}
+        />
+      </div>
+
+      <Button
+        type="primary"
+        shape="circle"
+        onClick={goNext}
+        icon={<VerticalLeftOutlined />}
+      />
+    </div>
+  )
+}
+
 export default function NavigationOverlay({
   setPageIndex,
   pageCount,
@@ -20,41 +65,15 @@ export default function NavigationOverlay({
   const [showOverlay, setShowOverlay] = useState(true)
   const ref = useRef<HTMLDivElement>(null)
 
-  const goNext = useCallback(() => {
-    setPageIndex(Math.min(pageCount - 1, pageIndex + 1))
-  }, [setPageIndex, pageIndex, pageCount])
-
-  const goPrev = useCallback(() => {
-    setPageIndex(Math.max(0, pageIndex - 1))
-  }, [setPageIndex, pageIndex])
-
   return (
     <div ref={ref} className="relative" style={props.dimensions}>
       <ConditionallyRender render={showOverlay}>
         <div className="absolute h-full w-full flex flex-col justify-end items-stretch z-10">
-          <div className="flex flex-row items-center">
-            <Button
-              type="primary"
-              shape="circle"
-              onClick={goNext}
-              icon={<VerticalRightOutlined />}
-            />
-
-            <div className="grow">
-              <Slider
-                value={pageIndex + 1}
-                onChange={(value) => setPageIndex(value - 1)}
-                max={pageCount}
-              />
-            </div>
-
-            <Button
-              type="primary"
-              shape="circle"
-              onClick={goPrev}
-              icon={<VerticalLeftOutlined />}
-            />
-          </div>
+          <Controls
+            pageCount={pageCount}
+            pageIndex={pageIndex}
+            setPageIndex={setPageIndex}
+          />
         </div>
       </ConditionallyRender>
 
