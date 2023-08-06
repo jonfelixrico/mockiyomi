efbb09457aa1bd7ce79e571f781c24da4a91e62f
Felix <jonfelixrico@gmail.com>
2023-08-06 23:38:17 +0800

    Add stuff to button stubs

--- src/components/navigation-controls/NavigationControls.tsx
+++ src/components/navigation-controls/NavigationControls.tsx
@@ -1,9 +1,23 @@
 import { Dispatch } from 'react'
+import { Button } from 'antd'
+import { VerticalRightOutlined, VerticalLeftOutlined } from '@ant-design/icons'
 
 export default function NavigationControls(props: {
   pageUrls: string[]
   pageIndex: number
   setPageIndex: Dispatch<number>
 }) {
-  return <div>{/* TODO add content */}</div>
+  return (
+    <div>
+      <Button type="primary" shape="circle">
+        <VerticalRightOutlined />
+      </Button>
+
+      <div>{/* do something */}</div>
+
+      <Button type="primary" shape="circle">
+        <VerticalLeftOutlined />
+      </Button>
+    </div>
+  )
 }
