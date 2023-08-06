890889391065994f491a9f078d0107f33d2a1cec
Felix <jonfelixrico@gmail.com>
2023-08-07 00:03:41 +0800

    Adjust measurer

--- src/components/common/Measurer.tsx
+++ src/components/common/Measurer.tsx
@@ -1,12 +1,17 @@
+import { Dimensions } from '@/types/dimensions.interface'
 import { CSSProperties, ReactNode } from 'react'
 import { useMeasure } from 'react-use'
 
 export default function Measurer(props: {
   className?: string
   style?: CSSProperties
-  children: ReactNode
+  render: (dimensions: Dimensions) => ReactNode
 }) {
   const [ref, { width, height }] = useMeasure<HTMLDivElement>()
 
-  return <div ref={ref} className={props.className} style={props.style}></div>
+  return (
+    <div ref={ref} className={props.className} style={props.style}>
+      {props.render({ width, height })}
+    </div>
+  )
 }
