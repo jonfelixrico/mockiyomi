b28d72c1353ac1de0cbd9e7aa76b2b8e4e492c95
Felix <jonfelixrico@gmail.com>
2023-08-06 23:40:56 +0800

    Add functionality

--- src/components/navigation-controls/NavigationControls.tsx
+++ src/components/navigation-controls/NavigationControls.tsx
@@ -1,21 +1,33 @@
-import { Dispatch } from 'react'
+import { Dispatch, useCallback } from 'react'
 import { Button } from 'antd'
 import { VerticalRightOutlined, VerticalLeftOutlined } from '@ant-design/icons'
 
-export default function NavigationControls(props: {
+export default function NavigationControls({
+  pageUrls,
+  pageIndex,
+  setPageIndex,
+}: {
   pageUrls: string[]
   pageIndex: number
   setPageIndex: Dispatch<number>
 }) {
+  const goNext = useCallback(() => {
+    setPageIndex(Math.min(pageUrls.length - 1, pageIndex + 1))
+  }, [setPageIndex, pageIndex, pageUrls])
+
+  const goPrev = useCallback(() => {
+    setPageIndex(Math.max(0, pageIndex - 1))
+  }, [setPageIndex, pageIndex])
+
   return (
     <div>
-      <Button type="primary" shape="circle">
+      <Button type="primary" shape="circle" onClick={goNext}>
         <VerticalRightOutlined />
       </Button>
 
       <div>{/* do something */}</div>
 
-      <Button type="primary" shape="circle">
+      <Button type="primary" shape="circle" onClick={goPrev}>
         <VerticalLeftOutlined />
       </Button>
     </div>
