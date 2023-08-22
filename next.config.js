d971d723bfcf97e459b8ea96af12a21f4b96add6
Felix <jonfelixrico@gmail.com>
2023-08-23 01:56:56 +0800

    Fix the canvas.node thing for PDF.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.node$/,
+
+      use: [
+        {
+          loader: 'node-loader',
+        },
+      ],
+    })
+
+    return config
+  },
+}
 
 module.exports = nextConfig
