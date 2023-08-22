89971ce42ed8a0069f91d93fa7dd6f076705bf73
Felix <jonfelixrico@gmail.com>
2023-08-23 00:38:55 +0800

    Remove unused code

--- src/components/document-viewer/NavigationOverlay.tsx
+++ src/components/document-viewer/NavigationOverlay.tsx
@@ -1,64 +1,6 @@
 import { Dimensions } from '@/types/dimensions.interface'
-import {
-  Dispatch,
-  ReactNode,
-  useCallback,
-  useMemo,
-  SetStateAction,
-} from 'react'
-import { Button, Slider } from 'antd'
-import { VerticalRightOutlined, VerticalLeftOutlined } from '@ant-design/icons'
+import { Dispatch, ReactNode, useCallback, SetStateAction } from 'react'
 import classnames from 'classnames'
-
-function Controls({
-  setPageIndex,
-  pageCount,
-  pageIndex,
-}: {
-  pageCount: number
-  pageIndex: number
-  setPageIndex: Dispatch<number>
-}) {
-  const goNext = useCallback(() => {
-    setPageIndex(Math.min(pageCount - 1, pageIndex + 1))
-  }, [setPageIndex, pageIndex, pageCount])
-
-  const goPrev = useCallback(() => {
-    setPageIndex(Math.max(0, pageIndex - 1))
-  }, [setPageIndex, pageIndex])
-
-  const setPageNo = useCallback(
-    (value: number) => setPageIndex(value - 1),
-    [setPageIndex]
-  )
-  const pageNo = useMemo(() => pageIndex + 1, [pageIndex])
-
-  return (
-    <div className="flex flex-row items-center gap-x-5">
-      <Button
-        type="primary"
-        shape="circle"
-        onClick={goPrev}
-        icon={<VerticalRightOutlined />}
-      />
-
-      <div>{pageNo}</div>
-
-      <div className="grow">
-        <Slider value={pageNo} onChange={setPageNo} max={pageCount} min={1} />
-      </div>
-
-      <div>{pageCount}</div>
-
-      <Button
-        type="primary"
-        shape="circle"
-        onClick={goNext}
-        icon={<VerticalLeftOutlined />}
-      />
-    </div>
-  )
-}
 
 export default function NavigationOverlay({
   showOverlay,
