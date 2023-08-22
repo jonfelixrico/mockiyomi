256239d305e23ca078f87e68df0f43221bb50dad
Felix <jonfelixrico@gmail.com>
2023-08-22 21:40:57 +0800

    Remove unused utils

--- src/utils/dom-utils.ts
+++
@@ -1,9 +0,0 @@
-export function isPointerEventWithinElement(
-  event: PointerEvent,
-  element: HTMLElement
-) {
-  return (
-    event.target &&
-    (event.target === element || element.contains(event.target as HTMLElement))
-  )
-}
