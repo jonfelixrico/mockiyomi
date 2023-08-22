b0f64b32aa87c9ebdf8f63ad69c3bea6a7d8cb18
Felix <jonfelixrico@gmail.com>
2023-08-23 01:57:38 +0800

    Add cmt

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
       test: /\.node$/,
 
       use: [
+        /*
+         * Needed for .node packages to work. Solution is taken from
+         * https://answers.netlify.com/t/canvas-node-webpack-isnt-working-with-node-loader/46600/3
+         */
         {
           loader: 'node-loader',
         },
