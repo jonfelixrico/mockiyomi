12fa571bc7e2660b933b52233582451b18868305
Felix <jonfelixrico@gmail.com>
2023-08-23 01:36:09 +0800

    Implement file upload

--- src/components/document-viewer/UploadControls.tsx
+++ src/components/document-viewer/UploadControls.tsx
@@ -1,10 +1,23 @@
 'use client'
 
+import { convertPDFToImageUrls } from '@/utils/pdf-utils'
 import { Button, Modal } from 'antd'
 import { useState } from 'react'
 
 export default function UploadControls() {
   const [isModalOpen, setIsModalOpen] = useState(false)
+
+  async function handleUpload(evt: InputEvent) {
+    const file = evt.dataTransfer?.files[0]
+
+    if (!file) {
+      console.log('no file')
+      return
+    }
+
+    const urls = await convertPDFToImageUrls(await file.arrayBuffer())
+    console.log(urls) // TODO change this
+  }
 
   return (
     <>
@@ -17,7 +30,9 @@ export default function UploadControls() {
         onOk={() => setIsModalOpen(false)}
         onCancel={() => setIsModalOpen(false)}
         title={'Test'}
-      ></Modal>
+      >
+        <input type="file" accept=".pdf" onInput={handleUpload} />
+      </Modal>
     </>
   )
 }
