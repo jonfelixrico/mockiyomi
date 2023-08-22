9c4b49f52d83ed05d3381673789480dbc4f79263
Felix <jonfelixrico@gmail.com>
2023-08-23 00:44:08 +0800

    Conditionally render only if the children is present

--- src/components/document-viewer/OverlayLayout.tsx
+++ src/components/document-viewer/OverlayLayout.tsx
@@ -1,6 +1,7 @@
 import { Dimensions } from '@/types/dimensions.interface'
 import { Dispatch, ReactNode, useCallback, SetStateAction } from 'react'
 import classnames from 'classnames'
+import ConditionallyRender from '../common/ConditionallyRender'
 
 export default function OverlayLayout({
   showOverlay,
@@ -25,34 +26,40 @@ export default function OverlayLayout({
 
   return (
     <div className="relative" style={dimensions}>
-      <div className="absolute h-full w-full flex flex-col justify-between items-stretch z-10 pointer-events-none">
-        <div
-          className={classnames(
-            'px-5 py-3 bg-white border-b border-gray-300 transition-transform',
-            {
-              'pointer-events-auto': showOverlay,
-            }
-          )}
-          style={{
-            transform: `translateY(${showOverlay ? 0 : -100}%)`,
-          }}
-        >
-          {topChildren}
-        </div>
+      <div className="absolute h-full w-full flex flex-col items-stretch z-10 pointer-events-none">
+        <ConditionallyRender render={!!topChildren}>
+          <div
+            className={classnames(
+              'px-5 py-3 bg-white border-b border-gray-300 transition-transform',
+              {
+                'pointer-events-auto': showOverlay,
+              }
+            )}
+            style={{
+              transform: `translateY(${showOverlay ? 0 : -100}%)`,
+            }}
+          >
+            {topChildren}
+          </div>
+        </ConditionallyRender>
 
-        <div
-          className={classnames(
-            'px-5 py-3 bg-white border-t border-gray-300 transition-transform',
-            {
-              'pointer-events-auto': showOverlay,
-            }
-          )}
-          style={{
-            transform: `translateY(${showOverlay ? 0 : 100}%)`,
-          }}
-        >
-          {bottomChildren}
-        </div>
+        <div className="grow" />
+
+        <ConditionallyRender render={!!bottomChildren}>
+          <div
+            className={classnames(
+              'px-5 py-3 bg-white border-t border-gray-300 transition-transform',
+              {
+                'pointer-events-auto': showOverlay,
+              }
+            )}
+            style={{
+              transform: `translateY(${showOverlay ? 0 : 100}%)`,
+            }}
+          >
+            {bottomChildren}
+          </div>
+        </ConditionallyRender>
       </div>
 
       {/* Clicking on the content will toggle showing of the overlay */}
