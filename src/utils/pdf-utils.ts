de8cfabd52e685cdb0d0510d16f61d694c0d45b6
Felix <jonfelixrico@gmail.com>
2023-08-23 01:19:18 +0800

    Make the method return something

--- src/utils/pdf-utils.ts
+++ src/utils/pdf-utils.ts
@@ -1,10 +1,33 @@
 'use client'
 
-import pdfJsLib, { getDocument } from 'pdfjs-dist'
+import pdfJsLib, { PDFPageProxy, getDocument } from 'pdfjs-dist'
 
 pdfJsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.js'
 
 type GetDocumentParam = Parameters<typeof getDocument>[0]
+
+async function getPageImageAsBlob(page: PDFPageProxy): Promise<Blob> {
+  const viewport = page.getViewport({ scale: 1 })
+
+  const canvas = document.createElement('canvas')
+  canvas.width = viewport.width
+  canvas.height = viewport.height
+
+  await page.render({
+    canvasContext: canvas.getContext('2d') as CanvasRenderingContext2D,
+    viewport,
+  })
+
+  return await new Promise((resolve, reject) => {
+    canvas.toBlob((blob) => {
+      if (blob) {
+        resolve(blob)
+      } else {
+        reject(new Error('blank blob'))
+      }
+    })
+  })
+}
 
 export async function convertPDFToImageUrls(
   src: GetDocumentParam
@@ -13,10 +36,21 @@ export async function convertPDFToImageUrls(
   const documentData = await loadingTask.promise
 
   const pageCount = documentData.numPages
+  const urls: string[] = []
 
   for (let pageNumber = 1; pageNumber <= pageCount; pageNumber++) {
-    const pageData = await documentData.getPage(pageNumber)
+    let pageData: PDFPageProxy | null = null
+
+    try {
+      pageData = await documentData.getPage(pageNumber)
+      const pageBlob = await getPageImageAsBlob(pageData)
+      urls.push(URL.createObjectURL(pageBlob))
+    } finally {
+      if (pageData) {
+        pageData.cleanup()
+      }
+    }
   }
 
-  return [] // TODO remove this bogus output
+  return urls
 }
