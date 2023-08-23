8c4a09646c4d1d07fa1a35405c2405a1bcce6153
Felix <jonfelixrico@gmail.com>
2023-08-23 22:33:15 +0800

    Strip off the content

--- src/components/document-viewer/UploadControls.tsx
+++ src/components/document-viewer/UploadControls.tsx
@@ -1,37 +1,16 @@
 'use client'
 
-import { convertPDFToImageUrls } from '@/utils/pdf-utils'
-import { Button, Modal } from 'antd'
-import { FormEvent, useState } from 'react'
+import { Button } from 'antd'
 
 export default function UploadControls() {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  async function handleUpload(evt: FormEvent<HTMLInputElement>) {
-    const file = evt.currentTarget.files?.[0]
-    if (!file) {
-      console.log('no file')
-      return
-    }
-
-    const urls = await convertPDFToImageUrls(await file.arrayBuffer())
-    console.log(urls) // TODO change this
-  }
-
   return (
     <>
       <div>
-        <Button onClick={() => setIsModalOpen(true)}>Choose File</Button>
+        <Button>
+          {/* i18nize this */}
+          Upload a File
+        </Button>
       </div>
-
-      <Modal
-        open={isModalOpen}
-        onOk={() => setIsModalOpen(false)}
-        onCancel={() => setIsModalOpen(false)}
-        title={'Test'}
-      >
-        <input type="file" accept=".pdf" onInput={handleUpload} />
-      </Modal>
     </>
   )
 }
