abfc0640a1069efed4fed37118d287ed818b4380
Felix <jonfelixrico@gmail.com>
2023-08-24 20:31:03 +0800

    Integrate UploadProcessModal

--- src/components/document-viewer/UploadControls.tsx
+++ src/components/document-viewer/UploadControls.tsx
@@ -1,16 +1,23 @@
 'use client'
 
 import { Button } from 'antd'
+import UploadProcessModal from './UploadProcessModal'
+import { useState } from 'react'
+import ConditionallyRender from '../common/ConditionallyRender'
 
 export default function UploadControls() {
+  const [opened, setOpened] = useState(false)
+
   return (
     <>
-      <div>
-        <Button>
-          {/* i18nize this */}
-          Upload a File
-        </Button>
-      </div>
+      <Button>
+        {/* i18nize this */}
+        Upload a File
+      </Button>
+
+      <ConditionallyRender render={opened}>
+        <UploadProcessModal open={opened} onOk={() => {}} onCancel={() => {}} />
+      </ConditionallyRender>
     </>
   )
 }
